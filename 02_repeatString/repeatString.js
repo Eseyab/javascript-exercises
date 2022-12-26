e4f23d35f1e67a6d8cafd771587943f0b54e848f
Eseyab <esseyabrm@gmail.com>
2022-12-26 16:35:18 -0500

    passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, numberofRepeats) {
+    let repeatedString = "";
 
+    if (numberofRepeats < 0){
+        return "ERROR";
+    }
+
+    for(let i = 0; i < numberofRepeats; i++){
+        repeatedString += str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
