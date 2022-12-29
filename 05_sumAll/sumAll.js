b7ebe8f754339cad584dedabd89e65140d966ec1
Eseyab <esseyabrm@gmail.com>
2022-12-29 17:37:35 -0500

    passed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    // ERROR if either num is not of type Number
+    if (typeof firstNum != "number" || typeof secondNum != "number" ) {
+        return "ERROR";
+    }
 
+    let lowerNum = Math.min(firstNum, secondNum);
+    let higherNum = Math.max(firstNum, secondNum);
+    //ERROR if nums are negative
+    if (lowerNum < 0){
+        return "ERROR";
+    }
+
+
+    let sum = 0;
+    for (let i = lowerNum; i <= higherNum; i++){
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
