57f07be4a2709e4a9f1ae4b375878de3bf8bc883
Eseyab <esseyabrm@gmail.com>
2022-12-29 17:09:02 -0500

    passed tests, lessons: if looping and splicing from array, loop in reverse

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(argArray, ...theArgs) {
+    for (let i = 0; i < theArgs.length; i++){
+        // Loop backwards because it is splicing elements in place
+        // which shifts the index of elements
+        // the shift would cause us to skip some elements if looping forward
+        for (j = argArray.length; j >= 0; j--){
+            // if arg is present in array remove by splicing out
+            if (theArgs[i] === argArray[j]){
+                argArray.splice(j, 1);
+            }
+        }
+    }
+    return argArray;
 };
 
 // Do not edit below this line
