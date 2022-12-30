f6dadb8498c7fa57a9aea046b56ba340a5340319
Eseyab <esseyabrm@gmail.com>
2022-12-30 15:02:13 -0500

    passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    // leapYear -> divisible by 4
+    // not divisible by 100, unless also divisible by 400
+    if (givenYear % 4 === 0){
+        if (givenYear % 100 === 0 && !(givenYear % 400 === 0)){
+            return false;
+        }
+        return true;
+    }
+    
+    return false;
 };
 
 // Do not edit below this line
