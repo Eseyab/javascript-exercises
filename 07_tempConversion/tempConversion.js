4fd2a0d1b428c401f0c40872afcb5a506e75f831
Eseyab <esseyabrm@gmail.com>
2022-12-30 15:23:36 -0500

    tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let unroundedCelsius = (temperature - 32) * 5/9;
+  let roundedCelsius = Math.round(unroundedCelsius * 10) / 10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let unroundedFahrenheit = temperature * (9/5) + 32;
+  let roundedFahrenheight = Math.round(unroundedFahrenheit * 10) / 10;
+  return roundedFahrenheight;
 };
 
 // Do not edit below this line
