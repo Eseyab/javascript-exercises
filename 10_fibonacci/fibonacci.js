67d3f2df3e17d6168f21daf3d1fe55074d153b8f
Eseyab <esseyabrm@gmail.com>
2023-02-09 03:43:00 -0500

    passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    // return error if not convertible to a number or if number is less than 1
+    if(isNaN(Number(n)) || n < 1){
+        return "OOPS";
+    }
+    if (n == 1 || n == 2){
+        return 1;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
