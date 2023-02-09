1eb03c5f922217d90d1370244750776ae90f08fa
Eseyab <esseyabrm@gmail.com>
2023-02-09 04:33:59 -0500

    passed tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        let age;
 
+        // if still alive, use current year to find age
+        if (!currentPerson.yearOfDeath){
+            age = (new Date()).getFullYear() - currentPerson.yearOfBirth;
+        } else {
+            age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+        
+        let oldestAge; 
+        if (!oldestPerson.yearOfDeath){
+            oldestAge = (new Date()).getFullYear() - oldestPerson.yearOfBirth;
+        } else {
+            oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        }
+        
+        if(age > oldestAge){
+            oldestPerson = currentPerson;
+        }
+
+        return oldestPerson;
+    })
 };
 
 // Do not edit below this line
